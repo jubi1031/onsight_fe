00400105088613698a574688ae399dceae20dd9d
jubi <joobe1031@gmail.com>
2024-09-23 23:46:50 +0900

    인증정보 포함하여 전송

--- src/pages/Oauth.jsx
+++ src/pages/Oauth.jsx
@@ -62,6 +62,7 @@ function Oauth() {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ kakaoInfo }),
+          credentials: 'include',
         });
 
         const data = await response.json();
